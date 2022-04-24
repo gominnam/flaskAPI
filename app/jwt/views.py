9189d49561a386e734e62c0b1bbacc1940f40df5
gominnam <kosok03@naver.com>
2022-04-24 15:20:12 +0900

    update jwt views

--- app/jwt/views.py
+++ app/jwt/views.py
@@ -4,22 +4,24 @@ from flask_restx import Namespace, Resource
 
 from app.common import status
 
-jwt_app = Blueprint('jwt_app', __name__, url_prefix='/jwt')
-jwt_api = Namespace('jwt_api', path='/jwt')
+token_app = Blueprint('token_app', __name__, url_prefix='/token')
+token_api = Namespace('token_api', path='/token')
 
 
-@jwt_api.route('/protected')
+@token_api.route('/protected')
+@token_api.header('Authorization: Bearer', 'JWT ACCESS TOKEN', required=True)
 class protected(Resource):
     @jwt_required()
     def get(self):
         current_user = get_jwt_identity()
-        return jsonify(logged_in_as=current_user), status.HTTP_200_OK
+        return {"logged_in_as": current_user}, status.HTTP_200_OK
 
 
-@jwt_api.route('/refresh')
+@token_api.route('/refresh')
+@token_api.header('Authorization: Bearer', 'JWT REFRESH TOKEN', required=True)
 class refresh(Resource):
     @jwt_required(refresh=True)
-    def get(self):
+    def post(self):
         current_user = get_jwt_identity()
         access_token = create_access_token(identity=current_user)
-        return jsonify(access_token=access_token, current_user=current_user)
+        return {"access_token": access_token, "current_user": current_user}, status.HTTP_200_OK
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from datetime import timedelta
 
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, verify_jwt_in_request
 from flask_restx import Api
 from sentry_sdk.integrations.flask import FlaskIntegration
 from settings.secrets import read_secret
@@ -25,12 +25,12 @@ app = Flask(__name__,
             template_folder='templates')
 
 app.register_blueprint(view.users_app)
-app.register_blueprint(jwt.jwt_app)
+app.register_blueprint(jwt.token_app)
 api = Api(app, version='1.0', title='API 문서', description='Swagger 문서', doc="/api-docs")
 
 api.add_namespace(view.users_api)
-api.add_namespace(jwt.jwt_api)
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+api.add_namespace(jwt.token_api)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.config['JWT_SECRET_KEY'] = "super-secret"  # Change this!
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=15)
